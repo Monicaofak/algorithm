df6e7b7215b7a93853d27b822280f347aae35252
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-01-06 11:33:47 +0800

    Create arithmetic expression.cpp

---
+++ algorithm competition accidence/1 chapter/arithmetic expression.cpp
@@ -0,0 +1,40 @@
+//计算并输出一些值
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+ int n=1+2;
+ int m=3-4;
+ int l=5*6;
+ int g=8/4;
+ int h=8/5;
+ cout<<n<<endl<<m<<endl<<l<<endl<<g<<endl<<h;
+}
+
+
+
+//计算并输出8除以5的值，保留小数点后一位
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+ double n=8.0/5.0;
+ cout<<n;
+}
+//  整数/整数=整数 浮点数/浮点数=浮点数，规则同样适用于加法、减法、除法
+
+
+//复杂的算术表达式
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+ double n =1+2*sqrt(3)/5-0.1;
+ cout<<n;
+}
+//整数-浮点数=浮点数 （整数先变浮点数，再减浮点数）
+
+
