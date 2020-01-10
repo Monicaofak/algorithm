cf844f2c76ee5af9d4c87e21db6441a7c739e315
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-01-10 10:56:46 +0800

    Create 2005.cpp

---
+++ Hduoj/2005.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+ double t;
+ while(cin>>t)
+ {
+ if(0 <= t&&t <=59)
+    cout<<"E"<<endl;
+ if(60 <=t&&t <=69)
+    cout<<"D"<<endl;
+ if(70 <=t&&t <=79)
+    cout<<"C"<<endl;
+ if(80 <=t&&t <=89)
+    cout<<"B"<<endl;
+ if(90 <=t&&t <=100)
+    cout<<"A"<<endl;
+ if(t<0||t>100)
+    cout<<"Score is error!"<<endl;
+ }
+ return 0;
+ }
