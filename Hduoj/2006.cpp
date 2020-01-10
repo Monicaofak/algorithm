b8dfb01ba7790024417696e1045e304a3654899b
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-01-10 12:29:26 +0800

    amass of odd numbers

--- Hduoj/2006.cpp
+++ Hduoj/2006.cpp
@@ -1,1 +1,24 @@
+//give you some integer,calculate the amass of odd numbers in them.
+#include <iostream>
+using namespace std;
+int main()
+ {
+ double t;
+ while(cin>>t)
+ {
+ if(0 <= t&&t <=59)
+    cout<<"E"<<endl;
+ if(60 <=t&&t <=69)
+    cout<<"D"<<endl;
+ if(70 <=t&&t <=79)
+    cout<<"C"<<endl;
+ if(80 <=t&&t <=89)
+    cout<<"B"<<endl;
+ if(90 <=t&&t <=100)
+    cout<<"A"<<endl;
+ if(t<0||t>100)
+    cout<<"Score is error!"<<endl;
+ }
+ return 0;
+ }
 
