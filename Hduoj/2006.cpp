aa5a22d9db60d828adcb253789cb991a11ac5616
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-01-11 10:01:43 +0800

    奇数的乘积

--- Hduoj/2006.cpp
+++ Hduoj/2006.cpp
@@ -1,4 +1,6 @@
-//give you some integer,calculate the amass of odd numbers in them.
+//给你n个整数，求他们中所有奇数的乘积。
+//输入数据包含多个测试实例，每个测试实例占一行，每行的第一个数为n，表示本组数据一共有n个，接着是n个整数，你可以假设每组数据必定至少存在一个奇数。
+//输出每组数中的所有奇数的乘积，对于测试实例，输出一行。
 #include<iostream>
 using namespace std;
 int main()
